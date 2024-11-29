ecd5800a68c0da4631ae3b9bb41e179927fced94
karthikdlr <Karthikdlr.598@gmail.com>
2024-11-29 13:40:51 +0530

    cache code changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, s-maxage=60, stale-while-revalidate=30',
+            },
+          ],
+        },
+      ];
+    },
+  };
 export default nextConfig;
