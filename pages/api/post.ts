c64deffe998043cbd51c8c904c1c358308f11d24
karthikdlr <Karthikdlr.598@gmail.com>
2024-11-29 17:51:50 +0530

    header cache changes

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -9,10 +9,13 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   const data = await fetch('https://api.restful-api.dev/objects/ff808181932badb6019376962b8347ac',{method: 'GET',
-     headers: {'Content-Type': 'application/json'}
+     headers: {
+      'Content-Type': 'application/json'}
     })
 
   const posts = await data.json()
-  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=30');
+  //res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=30');
+  res.setHeader('Vercel-CDN-Cache-Control', 'max-age=3600');
+
   res.status(200).json({ message: posts })
 }
