5af510bf5313467aeaaccaa99ace791b97307ce9
karthikdlr <Karthikdlr.598@gmail.com>
2024-11-29 18:15:44 +0530

    header changes

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -15,7 +15,7 @@ export default async function handler(
 
   const posts = await data.json()
   //res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=30');
-  res.setHeader('Vercel-CDN-Cache-Control', 'max-age=3600');
+  res.setHeader('Vercel-CDN-Cache-Control', 'max-age=60');
 
   res.status(200).json({ message: posts })
 }
